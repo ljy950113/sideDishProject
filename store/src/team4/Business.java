294fbf943d4ea3d1516237751d4b0ee23acaaa19
Leejeongyeon <failof95@naver.com>
2021-03-26 09:37:57 +0900

    Add files via upload
    03/26

--- store/src/team4/Business.java
+++ store/src/team4/Business.java
@@ -1,38 +1,14 @@
 package team4;
 
-
-
 public class Business {
 
 	public static void main(String[] args) {
 		
 		Buyer buyer = new Buyer();
 		Manager manager = new Manager();
-		
+
 		manager.start();
-		
-		
-		
 
-		
-		
-		
-		
-		
-			
-			
-				
-			
-			
-			
-		
-		
-
-		
-		
-		
-		
-		
 	}
 
 }
--- store/src/team4/Manager.java
+++ store/src/team4/Manager.java
@@ -2,202 +2,252 @@ package team4;
 
 import java.util.Scanner;
 
-import javax.swing.plaf.synth.SynthSpinnerUI;
-
-public class Manager extends Calculator implements Food{
-
+public class Manager implements Food{
 	Scanner sc = new Scanner(System.in);
-
-
-
-	int pwTest; //비밀번호를 입력받는 변수
-	//String[][] menuName = { {"무말랭이", "메추리알", "콩자반", "멸치볶음", "배추김치"}, {"갈비탕", "육개장", "된장찌개", "김치찌개", "부대찌개"} };//메뉴 이름 2차원배열
+	
 	String[] soupName = {"갈비탕", "육개장", "된장찌개", "김치찌개","부대찌개"}; // 탕종류 배열 0열에 육개장 1열에 김치찌개 이런식
-	int[] soupPrice = new int[5];
-	int[] soupAmount = new int[5];
-
+	int[] soupPrice = new int[6];
+	int[] soupAmount = new int[6]; // 0갈비탕 1육개장 2된장찌개 3김치찌개 4부대찌개
 	String[] sideName = {"무말랭이", "메추리알", "콩자반", "멸치볶음","배추김치"};// 반찬류 배열 0열에 배추김치, 1열에 갓김치
-	int[] sidePrice  = new int[5];
-	int[] sideAmount = new int[5];
+	int[] sidePrice  = new int[6];
+	int[] sideAmount = new int[6];
+	int[] remove = new int[6];
+	int[] rest1 = new int[6]; // 국 잔여량
+	int[] rest2 = new int[6]; // 반찬 잔여량
+	
+	boolean flag = true;
+	
+	public void start() {
 
-
-
-
-
-
-	@Override
-	public void search() { //제품 현황 조회
-		System.out.println("<< 제품 조회 >>");
-		System.out.println();
-		System.out.println("메뉴명  |  메뉴수량  |  메뉴가격  |  폐기갯수  | 메뉴평점");
-		System.out.println("-----------------------------------");
-		for(int i=0; i<sideName.length;i++) {
-			System.out.println(sideName[i] + " | " + sideAmount[i] + " | " + sidePrice[i] +  "|" + "폐기갯수" + " | " + "반찬평점");
-			//System.out.println("------------------------------------------------------------------------");
-			//System.out.println(soupName[i] + " | " + soupAmount[i] + " | " + "폐기갯수" + " | " + "반찬평점");
-		}
-		System.out.println("------------------------------------");
-		for(int i=0; i<soupName.length;i++) {
-			System.out.println(soupName[i] + " | " + soupAmount[i] + " | " + soupPrice[i] +  "|" + "폐기갯수" + " | " + "반찬평점");
-		}
-		managerMenu();
-
-	}//end search
-
-
-
-	void managerMenu() {
-
-		System.out.println("<< 관리자 모드 >>");
-		System.out.println();
-		System.out.println("1. 입고");
-		System.out.println();
-		System.out.println("2. 삭제");
-		System.out.println();
-		System.out.println("3. 제품 조회");
-		System.out.println();
-		System.out.println("4. 고객 관리");
-		System.out.println();
-		System.out.println("5. 뒤로 가기");
-		System.out.print("선택 >");
-		choice = sc.nextInt();
-
-		switch(choice) {
-
-		case 1: entering();break;
-		case 2: break;
-		case 3: search();break;
-		case 4: break;
-		case 5: start(); break;
-		default : break;
-
-		}
-
-	}//end managerMenu
-
-
-
-	@Override
-	public void password() { //관리자 비밀번호 입력 메소드
-
-		int repeat = 1;//반복횟수 담는 변수
-
-		for(int k = 0; k<repeat; k++) {
-
-			System.out.print("패스워드를 입력하세요 >");
-			pwTest = sc.nextInt();
-			if(password == pwTest) {
-				System.out.println("관리자 입니다!!!");
-				managerMenu();
-
-			}else { 
-				System.out.println("패스워드가 올바르지 않습니다. 다시 입력해주세요");
-				repeat++;
-				if(repeat == 4) {
-					System.out.println("패스워드를 3회 이상 틀리셨습니다. 메인 화면으로 돌아갑니다");
-					start();
-					break;
+		Buyer buyer = new Buyer();
+		
+		do {
+			System.out.println("<<반찬가게 관리 시스템>>");
+			System.out.println("1. 관리자 모드");
+			System.out.println();
+			System.out.println("2. 고객 모드");
+			System.out.println();
+			System.out.println("3. 프로그램 종료");
+			System.out.println("선택 > ");
+			int select = sc.nextInt();
+			
+			switch(select) {
+			case 1: password();  break;
+			case 2: Buyer.buyerPrint(); break;
+			case 3: System.out.println("시스템을 종료합니다."); flag = false; break;
+			default : System.out.println("메뉴를 다시 선택하세요");
+			}
+		}while(flag);
+	}
+	
+	public void password() {
+			int comparition = 1;
+			for(int i=0; i < comparition; i++) {
+				System.out.println("패스워드를 입력해주세요");
+				int pw = sc.nextInt();
+				if(password == pw) {
+					System.out.println("관리자가 맞습니다.");
+					managerMode();
+				}else {
+					System.out.println("패스워드가 옳바르지 않습니다. 다시 입력하세요");
+					comparition++;
+					if(comparition ==4) {
+						System.out.println("3회 이상 틀렸습니다. 메인으로 돌아갑니다.");
+						start();
+						break;
+					}
 				}
 			}
-
-		}	
-	}//end password
-
-
-
-	@Override
+	}
+	public void managerMode() {
+			
+				System.out.println("<<관리자 모드>>");
+				System.out.println("1. 입고");
+				System.out.println("2. 삭제");
+				System.out.println("3. 제품조회");
+				System.out.println("4. 고객관리");
+				System.out.println("5. 돌아가기");
+				int select = sc.nextInt();
+				switch(select) {
+				case 1: entering();  break;
+				case 2: delete();	break;
+				case 3: search();	break;
+				case 4: customer();	break;
+				case 5: start();
+							break;
+				default : System.out.println("다시 입력해주세요"); break;
+				}
+			}
 	public void entering() {
 		boolean run = true;
-
-		String choice; //선택 테스트용 변수입니다.
-		while(run) {
-			System.out.println("<<  입고  >>");
+		
+			while(run) {
+			System.out.println("<<입고>>");
 			System.out.println("1. 국  2. 반찬");
 			System.out.println("선택>");
 			int select = sc.nextInt();
-
+			
 			switch(select) {
 			case 1: 
-				System.out.println("------------------<< 국류 입고 가능 목록  >>------------------");
-				System.out.println("1. 갈비탕 | 2. 육개장 | 3. 된장찌개 | 4. 김치찌개 | 5. 부대찌개 ");
-				for(int i = 0; i<soupName.length; i++) {
-					System.out.println(soupName[i] + "을(를) 입고하시겠습니까?");
-					System.out.print("Y/N  >");
-					choice = sc.next();
-					if(choice.equals("Y") || choice.equals("y")) {
-						System.out.print("수량 입력>");
+				System.out.println("1. " + soupName[0] +" | " +  " 2. " + soupName[1] + " | " + " 3. "  +soupName[2] + " | " + " 4. " +soupName[3] +" | " + 	" 5. " + soupName[4]);
+				System.out.println("선택>");
+				int selectNo = sc.nextInt();
+				for(int i=0; i < soupName.length; i++) {
+					if(selectNo == i+1) {
+						System.out.println((i+1)+ ". " +soupName[i]);
+						System.out.println("수량 입력>");
 						soupAmount[i] += sc.nextInt();
-						System.out.print("가격 입력>");
+						System.out.println("가격 입력>");
 						soupPrice[i] = sc.nextInt();
-					}else if(choice.equals("N") || choice.equals("n")) {
-
-					}else {
-						System.err.println("잘못된 선택입니다.");
-						break;
-					}
-
-				}
-				managerMenu();
-				break;
-				
-				/*System.out.println("반찬을 입고하시겠습니까?(Y/N)");
+					}//selectNo if문 끝
+					if(selectNo > soupName.length) {
+						System.out.println("1번부터 5번중에 선택해주세요");
+						entering();
+					}//6번찍었을때 돌아가기
+				}//for문
+				System.out.println("계속 하시겠습니까?(Y/N)");
 				String ys = sc.next();
-				if(ys.equals("y") || ys.equals("Y")) {
-
-				} else if(ys.equals("n") || ys.equals("N")) {
-					run = false;
-					managerMenu();
-					break;
-				} else {
-					System.err.println("<시스템오류> 관리자 모드로 돌아갑니다.");
-					managerMenu();
-					break;
-				}*/
-
-
+				if(ys.equals("n") || ys.equals("N")) {
+					run = false;managerMode();
+				}//ys 문
+			break;
 			case 2:
-				System.out.println("------------------<< 반찬류 입고 가능 목록  >>------------------");
-				System.out.println("1. 무말랭이 | 2. 메추리알 | 3. 콩자반 | 4. 멸치볶음 | 5.배추김치");
-				for(int i = 0; i<sideName.length; i++) {
-					System.out.println(sideName[i] + "을(를) 입고하시겠습니까?");
-					System.out.print("Y/N  >");
-					choice = sc.next();
-					if(choice.equals("Y") || choice.equals("y")) {
-						System.out.print("수량 입력>");
+				System.out.println("1. " + sideName[0] +" | " +  " 2. " + sideName[1] + " | " +
+						" 3. "  +sideName[2] + " | " + " 4. " +sideName[3] +" | " + 
+						" 5. " + sideName[4]);
+				System.out.println("선택>");
+				selectNo = sc.nextInt();
+				for(int i=0; i < sideName.length; i++) {
+					if(selectNo == i+1) {
+						System.out.println((i+1)+ ". " +sideName[i]);
+						System.out.println("수량입력>");
 						sideAmount[i] += sc.nextInt();
-						System.out.print("가격 입력>");
+						System.out.println("가격입력>");
 						sidePrice[i] = sc.nextInt();
-					}else if(choice.equals("N") || choice.equals("n")) {
-
-					}else {
-						System.err.println("잘못된 선택입니다.");
-						break;
 					}
-
+					if(selectNo > sideName.length) {
+						System.out.println("1번부터 5번중에 선택해주세요");
+						entering();
+						}//6번찍으면 돌아가기
+				}//for문끝
+				System.out.println("계속 하시겠습니까?(Y/N)");
+				  ys = sc.next();
+				if(ys.equals("n") || ys.equals("N")) {
+				run = false;
+				managerMode();
 				}
-				managerMenu();
 				break;
-				
-				
-				
-				
-				
-			default : System.out.println("잘못입력하셨습니다. 다시입력하세요"); break;
+			default : System.out.println("잘못입력하셨습니다. 다시 입력해주세요"); break;
 			}
+		}//while문 끝
+	}
+
+
+	public void delete() {
+		boolean run = true;
+		
+			while(run) {
+			System.out.println("<<삭제>>");
+			System.out.println("1 국  2. 반찬");
+			System.out.println("선택 > ");
+			int select = sc.nextInt();
+			
+			switch(select) {
+			case 1:
+				System.out.println("1. " + soupName[0] +" | " +  " 2. " + soupName[1] + " | " +
+											" 3. "  +soupName[2] + " | " + " 4. " +soupName[3] +" | " + 
+											" 5. " + soupName[4]);
+				System.out.println("선택>");
+				int selectNo = sc.nextInt();
+				for( int i=0; i < soupAmount.length; i++) {
+					if(selectNo == i+1) {
+						System.out.println((i+1)+ ". " +soupName[i]);
+						System.out.println("몇개 폐기 하실래요?");
+						remove[i] += sc.nextInt();
+						rest1[i] = soupAmount[i]-remove[i];
+						if(rest1[i] <=0) {
+							System.out.println("뺄수없습니다.");
+							rest1[i] = 0;
+							}
+						}//ifselct끝
+					if(selectNo > soupName.length) {
+					System.out.println("1번부터 5번중에 선택해주세요");
+					delete();
+					}//6번찍었을때
+				}//for문 끝
+				System.out.println("돌아가기-->(Y or N)");
+				String yn = sc.next();
+				if(yn.equals("y") || yn.equals("Y")) managerMode();
+				if(yn.equals("n") || yn.equals("N")) delete();
+				else System.out.println("잘못입력하셨습니다."); 	
+				break;
+			case 2:
+				System.out.println("1. " + sideName[0] +" | " +  " 2. " + sideName[1] + " | " +
+											" 3. " + sideName[2] + " | " + " 4. " +sideName[3] +" | " + 
+											" 5. " + sideName[4]);
+				System.out.println("선택>");
+					selectNo = sc.nextInt();
+				for( int i=0; i < sideAmount.length; i++) {
+					if(selectNo == i+1) {
+						System.out.println((i+1)+ ". " + sideName[i]);
+						System.out.println("몇개 폐기 하실래요?");
+						remove[i] += sc.nextInt();
+						rest2[i] = sideAmount[i]-remove[i];
+						if(rest2[i] <=0) {
+							System.out.println("뺄수없습니다.");
+							rest2[i] = 0;	}
+						}
+					if(selectNo > sideName.length) {
+						System.out.println("1번부터 5번중에 선택해주세요");
+						delete();
+					}//6번찍었을때
+			}//for문 끝
+			System.out.println("돌아가기-->(Y or N)");
+				yn = sc.next();
+			if(yn.equals("y") || yn.equals("Y")) managerMode();
+			if(yn.equals("n") || yn.equals("N")) delete();
+			else System.out.println("잘못입력하셨습니다."); 
+			break;
+			default : System.out.println("잘못입력하셨습니다."); delete(); break;
+			}//end switch
+		}//end while
+	}//end delete
+	
+	public void customer() {
+		System.out.println("<<고객관리>>");
+		System.out.println("고객No. | 총 포인트");
+		
+	}
+	public void search() {
+		int i;
+		System.out.println("---------------<<제품 조회>>----------------------------");
+		System.out.println("------------------<<국류>>-------------------------------");
+		System.out.println("반찬명 | 입고 수량 |  잔여 갯수 | 반찬평점");
+		for (  i = 0; i < soupName.length; i++) {
+			System.out.println( soupName[i] + " | " + soupAmount[i] + " | " + rest1[i] + " | " + "반찬평점");
 		}
-	}//end entering
-
-
-
-
-
-
-
-
-
-
-
-
+		
+		System.out.println();
+		System.out.println("------------------<<반찬류>>-----------------------------");
+		System.out.println("반찬명 | 반찬 수량 | 총 액수 | 잔여 갯수 | 반찬평점");
+	
+		for (  i = 0; i < sideName.length; i++) {
+			System.out.println(sideName[i] + " | " + sideAmount[i]  + " | " + rest2[i] + " | " + "반찬평점");
+		}
+		System.out.println();
+		
+		System.out.println("돌아가기-->");
+		String yn = sc.next();
+		if(yn.equals("y") || yn.equals("Y")) {
+			managerMode();
+			}else {
+				search();
+			}
+	}
+	public  void customerSearch() {
+		for(int i = 0; i < soupName.length; i++) {
+			System.out.println(soupName[i] + " | " + soupAmount[i] + " | " + soupPrice[i]);
+			System.out.println(sideName[i] + " | " + sideAmount[i] + " | " + sidePrice[i]);
+		}
+	}
 }
-
-
