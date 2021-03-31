2c232043f370b7d14c2baed96485ba54f9280716
name1125 <81159222+name1125@users.noreply.github.com>
2021-04-01 01:54:06 +0900

    Add files via upload

---
+++ Business3.java
@@ -0,0 +1,18 @@
+package team4prac;
+
+import java.util.Scanner;
+
+public class Business3 {
+	static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		
+		Manager3 mg = new Manager3();
+		Buyer3 by = new Buyer3();
+		mg.start();
+			
+		
+		
+
+	}
+
+}
