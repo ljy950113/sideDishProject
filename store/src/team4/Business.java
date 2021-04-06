621417258ae1fe67e78c6f666923977ef782a17d
name1125 <81159222+name1125@users.noreply.github.com>
2021-04-06 19:59:56 +0900

    Add files via upload

--- store/src/team4/Business.java
+++ store/src/team4/Business.java
@@ -3,27 +3,13 @@ package team4;
 import java.util.Scanner;
 
 public class Business {
-	
 	static Scanner sc = new Scanner(System.in);
-
 	public static void main(String[] args) {
 		
-		//매니저클래스에서 프로그램 시작
-		Manager manager = new Manager();
-		manager.start();
+		Manager mg = new Manager();
 		
+		mg.start();
 		
-		
-		
-		
-		
-
 	}
-	
-	
-
-	
-	
-	
 
 }
