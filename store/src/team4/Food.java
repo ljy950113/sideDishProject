bb7f18ecbd9558d92e353ecca6bd6382586f140e
name1125 <81159222+name1125@users.noreply.github.com>
2021-04-06 20:00:20 +0900

    Add files via upload

--- store/src/team4/Food.java
+++ store/src/team4/Food.java
@@ -3,29 +3,25 @@ package team4;
 public interface Food {
 	
 	
-	static int password = 1234; //관리자 비밀번호 상수
+	static int password = 1234; 
 	
 	
-	default void search() {
-		//조회 관련 디폴트메소드
-	}
+	abstract void search(); 
 	
 	default void entering() {
-		//입고 관련 디폴트메소드
+		
 	}
 	
 	default void delete() {
-		//삭제 관련 디폴트메소드
+		
 	}
 	
 	default void customer() {
-		//고객관리 관련 디폴트메소드
+		
 	}
 	
 	default void password() {
-		//관리자 암호입력 관련 디폴트메소드
+		
 	}
-	
-	
-	
+
 }
